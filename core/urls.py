f2b5dde60207a6999599fb8f74f09d3aab356a8e
AbednegoTenge <abednegotenge180@gmail.com>
2026-01-16 22:18:05 +0000

    setting up endpoints using default router

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from .views import signup, login
+from rest_framework.routers import DefaultRouter
+from .views import ExpenseViewSet, IncomeViewSet, AuthViewSet
 
+router = DefaultRouter()
+router.register(r'auth', AuthViewSet, basename='auth')
+router.register(r'expenses', ExpenseViewSet, basename='expense')
+router.register(r'incomes', IncomeViewSet, basename='income')
 
-urlpatterns = [
-    path('signup/', signup),
-    path('login/', login),
-]
+urlpatterns = router.urls
